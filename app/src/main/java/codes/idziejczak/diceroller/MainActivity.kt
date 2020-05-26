583e87b675734665cb201ad9ef553c577566e78c
PanTajemnic <idziejczak3@gmail.com>
2020-05-26 21:29:55 +0200

    Add button functionality

--- app/src/main/java/codes/idziejczak/diceroller/MainActivity.kt
+++ app/src/main/java/codes/idziejczak/diceroller/MainActivity.kt
@@ -2,6 +2,7 @@ package codes.idziejczak.diceroller
 
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
@@ -16,7 +17,10 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun rollDice() {
-        Toast.makeText(this, "button clicked",
-            Toast.LENGTH_SHORT).show()
+//        Toast.makeText(this, "button clicked",
+//            Toast.LENGTH_SHORT).show()
+        val resultText: TextView = findViewById(R.id.result_text)
+        val randomInt = (1..6).random()
+        resultText.text = randomInt.toString()
     }
 }
