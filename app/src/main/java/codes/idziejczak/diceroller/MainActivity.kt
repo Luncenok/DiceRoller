5cdc3d3d05c5645625a7c34707e68d9a73136179
PanTajemnic <idziejczak3@gmail.com>
2020-05-26 21:41:02 +0200

    Count Up coding challenge

--- app/src/main/java/codes/idziejczak/diceroller/MainActivity.kt
+++ app/src/main/java/codes/idziejczak/diceroller/MainActivity.kt
@@ -3,7 +3,6 @@ package codes.idziejczak.diceroller
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -14,6 +13,8 @@ class MainActivity : AppCompatActivity() {
 
         val rollButton: Button = findViewById(R.id.roll_button)
         rollButton.setOnClickListener { rollDice() }
+        val countUpButton: Button = findViewById(R.id.count_up_button)
+        countUpButton.setOnClickListener { countUp() }
     }
 
     private fun rollDice() {
@@ -22,5 +23,14 @@ class MainActivity : AppCompatActivity() {
         val resultText: TextView = findViewById(R.id.result_text)
         val randomInt = (1..6).random()
         resultText.text = randomInt.toString()
+    }
+
+    private fun countUp() {
+        val resultText: TextView = findViewById(R.id.result_text)
+        if (resultText.text == "Hello World!") resultText.text = "1"
+        else {
+            val actualInt = resultText.text.toString().toInt()
+            if (actualInt != 6) resultText.text = (actualInt + 1).toString()
+        }
     }
 }
